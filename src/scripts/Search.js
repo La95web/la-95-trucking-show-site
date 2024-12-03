c0dc603659f3b80a2915496d2e806d84a9c53084
louis17782 <luissanteliz22@gmail.com>
2024-12-03 15:30:33 -0400

    configure pages and script

--- src/scripts/Search.js
+++ src/scripts/Search.js
@@ -1,19 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const searchInput = document.getElementById('site-search');
   const newsItems = document.getElementsByClassName('news-item');
-  
+  const noData = document.getElementById('result');
+
   searchInput.addEventListener('input', function() {
     const query = this.value.toLowerCase();
+    let found = false; 
     
     for (let newsItem of newsItems) {
       const containsQuery = newsItem.textContent.toLowerCase().includes(query);
       newsItem.style.display = containsQuery ? 'flex' : 'none';
+      if (containsQuery) {
+        found = true;
+      }
     }
 
     if (query === '') {
       for (let newsItem of newsItems) {
         newsItem.style.display = 'flex';
       }
+      noData.style.display = 'none';
+    } else {
+      noData.style.display = found ? 'none' : 'block'
     }
   });
 });
@@ -40,33 +48,64 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', adjustTruncation);
 });
 
+// button load more
 
-//links
+document.addEventListener('DOMContentLoaded', () => {
+  let itemsToShow = 2;
+  const loadMoreButton = document.getElementById('load-more');
+  const contentItems = document.querySelectorAll('.section-content-tips');
+
+  function showItems(count) {
+    let visibleItems = 0;
+    contentItems.forEach((item, index) => {
+      if (index < count) {
+        item.style.display = 'flex';
+        visibleItems++;
+      } else {
+        item.style.display = 'none';
+      }
+    });
+    if (visibleItems >= contentItems.length) {
+      loadMoreButton.innerText = 'No more';
+      loadMoreButton.disable = true; 
+    }
+  }
+
+  loadMoreButton.addEventListener('click', () => {
+    itemsToShow += 2;
+    showItems(itemsToShow);
+  });
+
+  showItems(itemsToShow);
+});
+
+
+
+
+//links news
 
 // document.addEventListener('DOMContentLoaded', () => {
-//   const noticiasLink = document.getElementById('link-noticias');
-//   const tipsLink = document.getElementById('link-tips');
-//   const dynamicContent = document.getElementById('dynamic-content');
+//   const noticiasButton = document.getElementById('button-noticias');
+//   const tipsButton = document.getElementById('button-tips');
+//   const noticiasContent = document.getElementById('content-noticias');
+//   const tipsContent = document.getElementById('content-tips');
 
-//   noticiasLink.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     loadContent('/noticias/');
+//   noticiasButton.addEventListener('click', () => {
+//     showContent(noticiasContent);
 //   });
 
-//   tipsLink.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     loadContent('/tips/');
+//   tipsButton.addEventListener('click', () => {
+//     showContent(tipsContent);
 //   });
 
-//   function loadContent(url) {
-//     fetch(url)
-//       .then(response => response.text())
-//       .then(data => {
-//         const parser = new DOMParser();
-//         const doc = parser.parseFromString(data, 'text/html');
-//         const newContent = doc.querySelector('#dynamic-content').innerHTML;
-//         dynamicContent.innerHTML = newContent;
-//       })
-//       .catch(error => console.error('Error al cargar el contenido:', error));
+//   function showContent(contentToShow) {
+//     // Ocultar todos los contenidos
+//     document.querySelectorAll('.content-item').forEach(content => {
+//       content.style.display = 'none';
+      
+//     });
+
+//     // Mostrar el contenido seleccionado
+//     contentToShow.style.display = 'block';
 //   }
 // });
