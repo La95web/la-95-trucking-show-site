68561ab5c1914e344b644c7361e4314669a5de3a
louis17782 <luissanteliz22@gmail.com>
2024-12-27 11:10:01 -0400

    configure functions

---
+++ src/scripts/ModalCart.js
@@ -0,0 +1,101 @@
+//modal
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openModalButtons = document.querySelectorAll('.openModal');
+  const modals = document.querySelectorAll('.modal');
+  const closeModalButtons = document.querySelectorAll('.close');
+
+  openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modalId = button.getAttribute('data-modal');
+      const modal = document.getElementById(modalId);
+      if (modal) {
+        modal.style.display = 'block';
+      } else {
+        console.error(`Modal con ID ${modalId} no encontrado.`);
+      }
+    });
+  });
+
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal');
+      if (modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
+
+  window.addEventListener('click', (event) => {
+    modals.forEach(modal => {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
+});
+
+// Gallery
+document.addEventListener('DOMContentLoaded', () => {
+  const slider = document.getElementById('gallerySlider');
+  const dots = document.querySelectorAll('.dot');
+  let currentIndex = 0;
+
+  function updateGallery() {
+    const width = slider.clientWidth;
+    slider.style.transform = `translateX(-${currentIndex * width}px)`;
+
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentIndex].classList.add('active');
+  }
+
+  function setIndex(index) {
+    currentIndex = index;
+    updateGallery();
+  }
+
+  dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+      setIndex(dot.getAttribute('data-index'));
+    });
+  });
+
+  document.getElementById('leftArrow').addEventListener('click', () => {
+    if (currentIndex > 0) {
+      setIndex(currentIndex - 1);
+    }
+  });
+
+  document.getElementById('rightArrow').addEventListener('click', () => {
+    if (currentIndex < dots.length - 1) {
+      setIndex(currentIndex + 1);
+    }
+  });
+
+  let touchStartX = 0;
+  let touchEndX = 0;
+
+  slider.addEventListener('touchstart', (e) => {
+    touchStartX = e.changedTouches[0].screenX;
+  });
+
+  slider.addEventListener('touchend', (e) => {
+    touchEndX = e.changedTouches[0].screenX;
+    handleGesture();
+  });
+
+  function handleGesture() {
+    if (touchEndX < touchStartX) {
+      if (currentIndex < dots.length - 1) {
+        setIndex(currentIndex + 1);
+      }
+    }
+    if (touchEndX > touchStartX) {
+      if (currentIndex > 0) {
+        setIndex(currentIndex - 1);
+      }
+    }
+  }
+
+  updateGallery();
+});
