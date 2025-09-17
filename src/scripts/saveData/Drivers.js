3e459d6ba5d7b99c8dc3e479435b1a80b6e96e8d
louis17782 <luissanteliz22@gmail.com>
2025-09-17 13:45:06 -0400

    configure functions

--- src/scripts/saveData/Drivers.js
+++ src/scripts/saveData/Drivers.js
@@ -10,7 +10,9 @@ document.querySelectorAll('#contact-form, #contact-form-b').forEach(form => {
       phone: formData.get('phone')
     };
 
-    fetch('http://localhost:3000/userDrivers', {
+    const API_URL = `${import.meta.env.PUBLIC_JOB_BASE_URL}/userDrivers`;
+
+    fetch(API_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data)
